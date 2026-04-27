aff762417202620e896ff09a2a4cc1f7c6e17b68
Abdul Rehman <63417636+AbdulRehman164@users.noreply.github.com>
2026-04-27 12:03:25 +0500

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,23 @@
+import math
+
+
+class Solution:
+    def isValid(self, num, hours, piles):
+        for pile in piles:
+            hours -= math.ceil(pile / num)
+            if hours < 0:
+                return False
+        return True
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        piles.sort()
+
+        L = 1
+        R = piles[-1]
+        while L <= R:
+            mid = (L + R) // 2
+            if self.isValid(mid, h, piles):
+                R = mid - 1
+            else:
+                L = mid + 1
+        return L
