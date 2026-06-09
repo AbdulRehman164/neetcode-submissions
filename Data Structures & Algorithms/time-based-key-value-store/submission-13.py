d4f4c14c57beca6d65e5a5d93f52768ac70ac435
Abdul Rehman <63417636+AbdulRehman164@users.noreply.github.com>
2026-06-09 09:58:39 +0500

    Add: time-based-key-value-store - submission-13

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-13.py
@@ -0,0 +1,26 @@
+from typing import DefaultDict
+
+
+class TimeMap:
+    def __init__(self):
+        self.data = DefaultDict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.data[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.data:
+            return ""
+        arr = self.data[key]
+        L = 0
+        R = len(arr) - 1
+
+        while L <= R:
+            mid = (L + R) // 2
+            if arr[mid][1] > timestamp:
+                R = mid - 1
+            elif arr[mid][1] < timestamp:
+                L = mid + 1
+            else:
+                return arr[mid][0]
+        return arr[R][0] if R >= 0 else ""
